b03522c192ea171b3eaba753c9df7b378dae525f
Sunjae <nalalasjsj@gmail.com>
2021-04-02 09:37:35 +0900

    1. Graph에 graph1, graph2 관련코드 수정(GraphController, GraphDao, GraphService,GraphServiceImpl, GraphVO)

--- src/main/java/com/project/wellness/admin/service/GraphService.java
+++ src/main/java/com/project/wellness/admin/service/GraphService.java
@@ -8,5 +8,6 @@ public interface GraphService {
 
 public List<GraphVO> selectGraph() throws Exception;
 public List<GraphVO> graph1() throws Exception;
-	
+public List<GraphVO> graph2f() throws Exception;
+public List<GraphVO> graph2m() throws Exception;
 }
