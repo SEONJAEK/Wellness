48ca98f6abc247f71180a8c40afb5f7f3fa6d8c2
GnorLab <choronglee.dev@gmail.com>
2021-04-03 20:47:12 +0900

    ReservViewController, ReservViewVO 생성

---
+++ src/main/java/com/project/wellness/reservView/controller/ReservViewController.java
@@ -0,0 +1,13 @@
+package com.project.wellness.reservView.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ReservViewController {
+	@RequestMapping(value="schedule.do",method=RequestMethod.GET)
+	   public String graph() {
+	      return "schedule";
+	   }
+}
