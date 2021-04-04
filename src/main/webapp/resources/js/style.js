6a2c78e7b7b60dbf350ace5346dbc88c2902a137
GnorLab <choronglee.dev@gmail.com>
2021-04-04 17:15:37 +0900

    style.js 슬라이더 코드 순서 수정

--- src/main/webapp/resources/js/style.js
+++ src/main/webapp/resources/js/style.js
@@ -42,27 +42,6 @@ $(document).ready(function() {
   });
 });
 
-// 슬라이드
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-       slides[i].style.display = "none";  
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}    
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 5000); // Change image every 2 seconds
-}
-
 //모바일 토글 버튼
 $(document).ready(function(){
   $(".mobile-nav").hide();
@@ -97,4 +76,25 @@ function initMap() {
     map: map,
     title: 'Wellness'
   });
+}
+
+// 슬라이드
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
